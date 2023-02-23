d6571f0cda0da34a50d1f3104f530342360d425d
LidorKY <118388214+LidorKY@users.noreply.github.com>
2023-02-23 20:51:09 +0200

    In general it works
    small letters: need to fix the timing because sometimes it works and sometimes not and need to add feature with files.

--- DHCPclient.py
+++ DHCPclient.py
@@ -1,7 +1,6 @@
 from random import randint
-
+import socket
 from scapy.all import *
-
 from scapy.layers import dhcp
 from scapy.layers.dhcp import DHCP, BOOTP
 from scapy.layers.dns import DNS
@@ -9,32 +8,122 @@ from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether, ARP
 
 
-def dicover_packet():
+def client_connection():
+
+    print("Client is running...\n")
+
+    discover_packet()
+
+    off_packet = sniff(count=1, filter='udp and (port 67 or port 68)')
+
+    request_packet(off_packet)
+
+    ack_pack = sniff(count=1, filter='udp and (port 67 or port 68)')
+
+
+def discover_packet():
+    #------------Ethernet Layer--------------#
     ether = Ether()
-    ether.dst = "08:00:27:d3:c1:ef"
-    ether.show()
+    ether.src = "00:11:22:33:44:55" #can also use RandMAC() function
+    #ether.show()
+    # ---------------------------------------#
 
+
+    # ------------IP Layer--------------#
     ip = IP()
-    ip.dst = "127.0.0.1"
-    ip.show()
+    ip.src = '0.0.0.0'
+    # ip.show()
+    # -----------------------------------#
 
+
+    # ------------Transport Layer--------------#
     udp = UDP()
     udp.sport = 67
     udp.dport = 68
     udp.show()
+    # -----------------------------------------#
 
+
+    # ------------Didn't fully understand this Layer--------------#
     bootp = BOOTP()
+    bootp.ciaddr = ip.src
+    # bootp.yiaddr =
+    bootp.siaddr = "10.0.2.15"
+    # bootp.giaddr =
+    bootp.chaddr = "00:11:22:33:44:55"
     bootp.flags = 1
-    udp.show()
+    # udp.show()
+    # ------------------------------------------------------------#
 
+
+    # ------------Application Layer--------------#
     dhcp = DHCP(options=[("message-type", "discover"), "end"])
-    dhcp.show()
+    # dhcp.show()
+    # -------------------------------------------#
 
+
+    # ------------The Complete Packet--------------#
     dhcp_discover = ether / ip / udp / bootp / dhcp
-    dhcp_discover.show()
+    # dhcp_discover.show()
+    # ---------------------------------------------#
+
 
     sendp(dhcp_discover)
 
 
+
+def request_packet(off_packet):
+    # ------------Ethernet Layer--------------#
+    ether = Ether()
+    ether.src = "00:11:22:33:44:55"
+    # ether.show()
+    # ----------------------------------------#
+
+
+    # ------------IP Layer--------------#
+    ip = IP()
+    ip.src = off_packet[0][3].yiaddr
+    ip.dst = off_packet[0][1].src
+    # ip.show()
+    # ----------------------------------#
+
+
+    # ------------Transport Layer--------------#
+    udp = UDP()
+    udp.sport = 67
+    udp.dport = 68
+    # udp.show()
+    # -----------------------------------------#
+
+
+    # ------------Didn't fully understand this Layer--------------#
+    bootp = BOOTP()
+    bootp.ciaddr = ip.src
+    # bootp.yiaddr =
+    bootp.siaddr = "10.0.2.15"
+    # bootp.giaddr =
+    bootp.chaddr = "00:11:22:33:44:55"
+    bootp.flags = 1
+    # udp.show()
+    # ------------------------------------------------------------#
+
+
+    # ------------Application Layer--------------#
+    dhcp = DHCP(options=[("message-type", "request"), "end"])
+    # dhcp.show()
+    # -------------------------------------------#
+
+
+    # ------------The Complete Packet--------------#
+    dhcp_request = ether / ip / udp / bootp / dhcp
+    # dhcp_request.show()
+    # ---------------------------------------------#
+
+
+    sendp(dhcp_request)
+
+
+
+
 if __name__ == "__main__":
-    dicover_packet()
+    client_connection()
--- DHCPserver.py
+++ DHCPserver.py
@@ -1,47 +1,125 @@
 from random import randint
-
+import socket
 from scapy.all import *
-
 from scapy.layers import dhcp
 from scapy.layers.dhcp import DHCP, BOOTP
 from scapy.layers.dns import DNS
 from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether, ARP
 
-def offer_packet():
+def server_connection():
+
+    print("Server is running...\n")
+
+    disco_packet = sniff(count=1, filter='udp and (port 67 or port 68)')
+
+    offer_packet(disco_packet)
+
+    req_packet = sniff(count=1, filter='udp and (port 67 or port 68)')
+
+    ack_packet(req_packet)
+
+
+def offer_packet(disco_packet):
+    # ------------Ethernet Layer--------------#
     ether = Ether()
-    ether.dst = "08:00:27:d3:c1:ef"
-    ether.show()
+    ether.src = "08:00:27:d3:c1:ef"
+    ether.dst = disco_packet[0][0].src
+    # ether.show()
+    # ----------------------------------------#
 
+
+    # ------------IP Layer--------------#
     ip = IP()
-    ip.dst = "127.0.0.1"
-    ip.show()
+    ip.src = '10.0.2.15'
+    ip.dst = disco_packet[0][1].src
+    # ip.show()
+    # -----------------------------------#
 
+
+    # ------------Transport Layer--------------#
     udp = UDP()
     udp.sport = 68
     udp.dport = 67
-    udp.show()
+    # udp.show()
+    # -----------------------------------------#
 
+
+    # ------------Didn't fully understand this Layer--------------#
     bootp = BOOTP()
     bootp.flags = 2
     bootp.yiaddr = "192.168.1.1"
-    udp.show()
+    bootp.sname = "I am the DHCP server"
+    # udp.show()
+    # ------------------------------------------------------------#
 
+
+    # ------------Application Layer--------------#
     dhcp = DHCP(options=[("message-type", "offer"), "end"])
-    dhcp.show()
+    # dhcp.show()
+    # -------------------------------------------#
 
+
+    # ------------The Complete Packet--------------#
     dhcp_offer = ether / ip / udp / bootp / dhcp
-    dhcp_offer.show()
+    # dhcp_offer.show()
+    # ---------------------------------------------#
+
 
     sendp(dhcp_offer)
+
+def ack_packet(req_packet):
+    # ------------Ethernet Layer--------------#
+    ether = Ether()
+    ether.src = "08:00:27:d3:c1:ef"
+    ether.dst = req_packet[0][0].src
+    # ether.show()
+    # ----------------------------------------#
+
+
+    # ------------IP Layer--------------#
+    ip = IP()
+    ip.src = '10.0.2.15'
+    ip.dst = req_packet[0][1].src
+    # ip.show()
+    # -----------------------------------#
+
+
+    # ------------Transport Layer--------------#
+    udp = UDP()
+    udp.sport = 68
+    udp.dport = 67
+    # udp.show()
+    # -----------------------------------------#
+
+
+    # ------------Didn't fully understand this Layer--------------#
+    bootp = BOOTP()
+    bootp.flags = 2
+    bootp.sname = "I am the DHCP server"
+    # udp.show()
+    # ------------------------------------------------------------#
+
+
+    # ------------Application Layer--------------#
+    dhcp = DHCP(options=[("message-type", "ack"), "end"])
+    # dhcp.show()
+    # -------------------------------------------#
+
+
+    # ------------The Complete Packet--------------#
+    dhcp_ack = ether / ip / udp / bootp / dhcp
+    # dhcp_offer.show()
+    # ---------------------------------------------#
+
+    sendp(dhcp_ack)
 
 
 
 
 
 if __name__ == "__main__":
-    offer_packet()
-
+    server_connection()
 
 
 
