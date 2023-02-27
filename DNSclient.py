f21e8003ed4da38fccff608848af88f539d74eac
LidorKY <118388214+LidorKY@users.noreply.github.com>
2023-02-27 17:56:31 +0200

    completely works

--- DNSclient.py
+++ DNSclient.py
@@ -1,5 +1,4 @@
 import random
-
 from scapy.all import DNS, DNSQR, IP, sr1, UDP
 from random import randint
 import socket
@@ -16,31 +15,42 @@ def client_connection():
     query_packet()
 
     response_packet = sniff(count=1, filter='udp port 53')
-    response_packet.show()
     print("client end")
+    return
 
 
 
 
 def query_packet():
+    #-----------------IP Layer---------------#
     ip = IP()
     ip.src = RandIP()
     ip.dst = '192.168.0.5'
+    #----------------------------------------#
 
+
+    #-----------------Transport Layer---------------#
     udp = UDP()
-    # udp.sport = 20230
     udp.dport = 53
+    #-----------------------------------------------#
 
+
+    #-----------------Application Layer---------------#
     dns = DNS()
     dns.id = random.randint(1, pow(2, 16)-1)
     dns.qr = 0
     dns.opcode = 0
     dns.rd = 1
-
     dns.qd = DNSQR()
     dns.qd.qname = input("Please enter the site name - example.com: \n")
+    # ------------------------------------------------#
 
+
+    #-----------------The Complete Packet---------------#
     dns_request = ip / udp / dns
+    #---------------------------------------------------#
+
+
 
     send(dns_request)
 
--- DNSserver.py
+++ DNSserver.py
@@ -1,5 +1,4 @@
-
-import nslookup as nslookup
+from time import sleep
 from scapy.all import DNS, DNSQR, IP, sr1, UDP, sendp, send, sniff, sndrcv
 from random import randint
 import socket
@@ -11,41 +10,112 @@ from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether, ARP
 
 
+my_dictionary = dict()
+
+
 def server_connection():
     print("Hello I am the DNS server")
-    que_packet = sniff(count=1, filter='udp port 53')
-    print("sniffed packet")
+    while True:
+        print("---sniffing---\n")
+        que_packet = sniff(count=1, filter='udp port 53')
+        print("sniffed packet\n")
+
+        if srever_cache(que_packet) == True:
+            print("from cache\n")
+        else:
+            sleep(1)
+            response_packet(que_packet)
+            print("server sent response\n")
+
+        if "stop" == input("if you want to stop pls write stop: \n"):
+            return
+
+    print("server end")
+    return
 
 
-    response_packet(que_packet)
-    print("server sent response")
+def srever_cache(que_packet):
+    if que_packet[0][3].qd.qname in my_dictionary:
+
+        # -----------------IP Layer---------------#
+        ip = IP()
+        ip.src = '192.168.0.5'
+        ip.dst = que_packet[0][1].src
+        # ----------------------------------------#
+
+
+        # -----------------Transport Layer---------------#
+        udp = UDP()
+        udp.sport = que_packet[0][2].dport
+        udp.dport = que_packet[0][2].sport
+        # -----------------------------------------------#
+
+
+        # -----------------Application Layer---------------#
+        dns = DNS()
+        dns.id = que_packet[0][3].id
+        dns.qr = 1
+        dns.rd = 1
+        dns.ra = 1
+        dns.qd = que_packet[0][3].qd
+        dns.an = DNSRR()
+        dns.an.rrname = que_packet[0][3].qd.qname
+        dns.an.rdata = my_dictionary[que_packet[0][3].qd.qname]
+        # ------------------------------------------------#
+
+
+        # -----------------The Complete Packet---------------#
+        dns_response = ip / udp / dns
+        # dns_response = dns_response.__class__(bytes(dns_response))
+        # ---------------------------------------------------#
+
+
+        send(dns_response)
+
+        return True
+
+    else:
+
+        return False
+
 
 
 def response_packet(que_packet):
+    # -----------------IP Layer---------------#
     ip = IP()
     ip.src = '192.168.0.5'
     ip.dst = que_packet[0][1].src
+    # ----------------------------------------#
 
+
+    # -----------------Transport Layer---------------#
     udp = UDP()
     udp.sport = que_packet[0][2].dport
     udp.dport = que_packet[0][2].sport
+    # -----------------------------------------------#
 
+
+    # -----------------Application Layer---------------#
     dns = DNS()
     dns.id = que_packet[0][3].id
     dns.qr = 1
     dns.rd = 1
     dns.ra = 1
-
     dns.qd = que_packet[0][3].qd
     dns.an = DNSRR()
     dns.an.rrname = que_packet[0][3].qd.qname
-    dns.an.rdata = socket.getaddrinfo(dns.an.rrname, 53)
-    dns.an.type = 'A'
+    dns.an.rdata = socket.getaddrinfo(dns.an.rrname, 53)[0][4][0].split()[0]
+    my_dictionary[que_packet[0][3].qd.qname] = dns.an.rdata
+    # ------------------------------------------------#
 
+
+    # -----------------The Complete Packet---------------#
     dns_response = ip / udp / dns
-    print("here")
-    send(dns_response)
+    # dns_response = dns_response.__class__(bytes(dns_response))
+    # ---------------------------------------------------#
 
+
+    send(dns_response)
 
 
 
